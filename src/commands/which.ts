ca760ac0344da6c1660f5d47cf1419db5649d358
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2022-11-02 14:40:05 -0400

    feat: work with different topicSeparators

--- src/commands/which.ts
+++ src/commands/which.ts
@@ -6,7 +6,7 @@ export default class Which extends Command {
 
   async run(): Promise<void> {
     const {argv} = await this.parse(Which)
-    const cmd = this.config.findCommand(argv.join(':'), {must: true})
+    const cmd = this.config.findCommand(argv.join(this.config.topicSeparator), {must: true})
     CliUx.ux.styledHeader(cmd.id)
     CliUx.ux.styledObject({
       plugin: cmd.pluginName,
