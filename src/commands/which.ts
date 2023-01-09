7a5fd968d146d366db9cfc7dfe33747402d6d5e4
Cristian Dominguez <cdominguez@salesforce.com>
2023-01-09 18:47:40 -0300

    fix: throw error if missing cmd arg

--- src/commands/which.ts
+++ src/commands/which.ts
@@ -3,6 +3,12 @@ import {Command, CliUx} from '@oclif/core'
 export default class Which extends Command {
   static description = 'Show which plugin a command is in.'
   static strict = false;
+  static examples = [
+    {
+      description: 'See which plugin the `help` command is in:',
+      command: '<%= config.bin %> <%= command.id %> help',
+    },
+  ]
 
   async run(): Promise<void> {
     const {argv} = await this.parse(Which)
@@ -12,6 +18,8 @@ export default class Which extends Command {
       // If this if statement is true then the command to find was passed in as a single string, e.g. `mycli which "my command"`
       // So we must use the topicSeparator to split it into an array
       command = argv[0].split(this.config.topicSeparator)
+    } else {
+      throw new Error('Missing 1 required arg')
     }
 
     const cmd = this.config.findCommand(command.join(':'), {must: true})
