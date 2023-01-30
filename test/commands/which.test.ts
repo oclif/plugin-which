90c3db345db6ba9574745bec9a29f250eb24f943
Cristian Dominguez <cdominguez@salesforce.com>
2023-01-30 18:00:28 -0300

    test: add UT

--- test/commands/which.test.ts
+++ test/commands/which.test.ts
@@ -7,4 +7,12 @@ describe('which', () => {
   .it('which which', ctx => {
     expect(ctx.stdout).to.contain('@oclif/plugin-which')
   })
+
+  test
+  .stdout()
+  .command(['which'])
+  .catch(error => {
+    expect(error.message).to.contain('"which" expects a command name.  Try something like "which your:command:here" ')
+  })
+  .it('checks arg was provided')
 })
