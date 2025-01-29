3106a0668abb46e1d0d5829a9a23582b7092ba59
Mike Donnalley <mdonnalley@salesforce.com>
2025-01-29 13:46:06 -0700

    chore(W-17692101): eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import {includeIgnoreFile} from '@eslint/compat'
+import oclif from 'eslint-config-oclif'
+import prettier from 'eslint-config-prettier'
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
+
+const gitignorePath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...oclif,
+  prettier,
+  {
+    files: ['test/**/*.ts'],
+    rules: {
+      'import/no-named-as-default-member': 'off',
+    },
+  },
+  {
+    files: ['test/fixtures/**/*.cjs'],
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+]
